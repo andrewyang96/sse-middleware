1a63ddbf5b7d67cdec3ab3d25c01756185298082
Andrew Yang <ayang14@illinois.edu>
2016-08-28 16:19:06 -0500

    Refactor tests

--- test/test-sse.js
+++ test/test-sse.js
@@ -4,15 +4,16 @@ var Response = require('./lib/mock-res');
 
 describe('SSE', function () {
   var sse;
+  var res;
 
   before(function () {
     sse = new SSE();
+    res = new Response();
   });
 
 	describe('#middleware', function () {
-    it('should properly handle requests and responses', function () {
+    it('should add response to connection list in response handler', function (done) {
       var middlewareFunc = sse.middleware.bind(sse);
-      var res = new Response();
       var nextWasCalled = false;
       middlewareFunc('req', res, function () {
         nextWasCalled = true;
@@ -23,19 +24,29 @@ describe('SSE', function () {
       assert.equal('no-cache', res.headers['Cache-Control']);
       assert.equal('keep-alive', res.headers['Connection']);
       assert.notEqual(-1, sse.connections.indexOf(res));
+      done();
+    });
 
+    it('should send plain data through response', function (done) {
       res.sendData('hello world');
       assert.equal('data: hello world\n\n', res.lastWritten);
+      done();
+    });
 
+    it('should send JSON data through response', function (done) {
       var obj = {
         'json': true,
         'answer': 42
       };
       res.sendJSON(obj);
       assert.equal('data: ' + JSON.stringify(obj) + '\n\n', res.lastWritten);
+      done();
+    });
 
+    it('should remove response from connection list once connection is closed', function (done) {
       res.eventEmitter.emit('close');
       assert.equal(-1, sse.connections.indexOf(res));
+      done();
     });
   });
 });
