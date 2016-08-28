a2cfc9a0e20f36426765aad6e6f557f9ca7bb4fe
Andrew Yang <ayang14@illinois.edu>
2016-08-28 14:56:33 -0400

    Update demo

--- demo/index.js
+++ demo/index.js
@@ -24,6 +24,15 @@ app.get('/randuuid', randUUIDSSE.middleware.bind(randUUIDSSE), function (req, re
   console.log('Connected to random UUID endpoint');
 });
 
+var randMultiplesSSE = new SSE('num');
+app.get('/randmultiples/:num', randMultiplesSSE.middleware.bind(randMultiplesSSE), function (req, res, next) {
+  if (['2', '3', '4', '5', '6', '7', '8', '9'].indexOf(req.params.num) === -1) {
+    next();
+  }
+  res.sendData(randMultiples[Number.parseInt(req.params.num)]);
+  console.log('Connected to random multiple endpoint: ', req.params.num);
+});
+
 /* Random value generation */
 var generateRandomDelay = function () {
   var delay = Math.floor(Math.random() * 3001) + 500;
@@ -54,6 +63,25 @@ var updateRandomUUID = function () {
   setTimeout(updateRandomUUID, generateRandomDelay());
 };
 setTimeout(updateRandomUUID, generateRandomDelay);
+
+var generateRandomMultiple = function (num) {
+  return Math.ceil(Math.random() * 10) * num;
+};
+var randMultiples = {};
+var updateMultiple = function (num) {
+  randMultiples[num] = generateRandomMultiple(num);
+  console.log('Updated multiple', num, 'to', randMultiples[num]);
+  randMultiplesSSE.sendData(randMultiples[num], num);
+  setTimeout(function () {
+    updateMultiple(num);
+  }, generateRandomDelay());
+};
+var populateMultiples = function () {
+  for (var i = 2; i <= 9; i++) {
+    randMultiples[i] = updateMultiple(i);
+  }
+};
+populateMultiples();
 
 /* Initialize server */
 app.listen(3000, function () {
--- demo/public/script.js
+++ demo/public/script.js
@@ -1,10 +1,44 @@
-var numSource = new EventSource('/randnum');
-var uuidSource = new EventSource('/randuuid');
+window.onload = function () {
+	var numSource = new EventSource('/randnum');
+  var uuidSource = new EventSource('/randuuid');
 
-numSource.addEventListener('message', function (e) {
-  document.getElementById('randNum').innerHTML = e.data;
-});
+  numSource.addEventListener('message', function (e) {
+    document.getElementById('randNum').innerHTML = e.data;
+  });
 
-uuidSource.addEventListener('message', function (e) {
-  document.getElementById('randUUID').innerHTML = e.data;
-});
+  uuidSource.addEventListener('message', function (e) {
+    document.getElementById('randUUID').innerHTML = e.data;
+  });
+
+  var toggleForm = document.getElementById('toggleForm');
+  toggleForm.onchange = function () {
+    var checkboxes = toggleForm.getElementsByTagName('input');
+    var activeCheckboxes = [];
+    for (var i = 0; i < checkboxes.length; i++) {
+      var checkbox = checkboxes[i];
+      if (checkbox.checked) {
+        activeCheckboxes.push(checkbox.value);
+      }
+    }
+    updateTracking(activeCheckboxes);
+  };
+
+  var multiplesTracking = document.getElementById('multiplesTracking');
+  var updateTracking = function (tracking) {
+    removeTrackingElements();
+    for (var i = 0; i < tracking.length; i++) {
+      createTrackingElement(tracking[i]);
+    }
+  };
+  var createTrackingElement = function (value) {
+    var element = '<h4>' + value + ': <span id="e' + value + '"></span></h4>';
+    multiplesTracking.innerHTML += element;
+    var source = new EventSource('/randmultiples/' + value);
+    source.addEventListener('message', function (e) {
+      multiplesTracking.querySelector('#e' + value).innerHTML = e.data;
+    });
+  };
+  var removeTrackingElements = function () {
+    multiplesTracking.innerHTML = '';
+  };
+};
